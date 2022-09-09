d002c4c21182312f6ded03d16136ff7139fcf17f
Fuechee <f9vang@gmail.com>
2022-09-09 17:13:28 -0500

    Created URLs + Created view Function for GET and POST

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from itertools import product
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers
 
-# Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+       
